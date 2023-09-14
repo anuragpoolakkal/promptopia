9666dfc8b6eb3856631a73a655696086917c5ae8
anuragrajanp <anuragrajanp@gmail.com>
2023-09-14 11:51:04 +0530

    fix favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,25 +4,28 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+	title: "Promptopia",
+	description: "Discover & Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang='en'>
-    <body>
-      <Provider>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
+	<html lang="en">
+		<head>
+			<link rel="icon" type="image/x-icon" href="favicon.ico" sizes="any" />
+		</head>
+		<body>
+			<Provider>
+				<div className="main">
+					<div className="gradient" />
+				</div>
 
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
-      </Provider>
-    </body>
-  </html>
+				<main className="app">
+					<Nav />
+					{children}
+				</main>
+			</Provider>
+		</body>
+	</html>
 );
 
 export default RootLayout;
